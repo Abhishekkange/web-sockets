6748c885674cc19ace544773dd80ba97485c4437
Abhishek <Abhishekkange@gmail.com>
2024-06-11 18:47:38 +0530

    Relay WebSocket Server created

--- code1/index.js
+++ code1/index.js
@@ -1,10 +1,37 @@
-const webSocket = require("ws");
+const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({  port :3000});
+var clients = [];
+const serverWS = new WebSocket.Server({ port: 3000 });
 
-wss.on("connection",()=>{
+//WSS stands for web socket server
+serverWS.on("connection", clientWS => {
 
-    console.log("new client connected");    
+    //check for maximum connections
+    if (clients.length >= 2) {
+        clientWS.close();
+    } else {
+
+        console.log("new client connected");
+        clients.push(clientWS);
+        // console.log(clientWS);
+    }
+
+  clientWS.on("message", message =>{
+
+    
+
+    clients.forEach(client =>{
+
+        if(client!=clientWS)
+            {
+                client.send(message.toString());
+                console.log(message.toString());
+            }
+    })
+
+
+  });
+
 
 });
 
